4af3baebf52f68ef6006d3c6460cfdc454d8568c
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-11 16:38:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
-const token = process.env.TOKEN:
+const token = process.env.TOKEN;
 
 fs.readdir("./src/commands/", (err, files) => {
   if (err) return console.error(err);
